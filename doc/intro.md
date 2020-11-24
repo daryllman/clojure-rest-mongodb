feeabef560ccfbf3cdfedab2b355737fb906ce11
Daryll Wong <daryll_wong@hotmail.com>
2020-11-24 12:55:37 +0800

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-rest-mongodb
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure_rest_mongodb/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-rest-mongodb.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-rest-mongodb.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
