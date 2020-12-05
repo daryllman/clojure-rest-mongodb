5d58057d332f5f75059eae8b7f6fd9ed9550c7a5
Daryll Wong <daryll_wong@hotmail.com>
2020-12-05 15:42:22 +0800

    update command

--- setup.sh
+++ setup.sh
@@ -10,6 +10,7 @@ unzip meta_kindle_store.zip
 sudo apt-get install gnupg
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt-get update
 sudo apt-get install -y mongodb-org
 sudo service mongod start
 mongoimport --db kindle --collection metadata --file meta_Kindle_Store.json --legacy
